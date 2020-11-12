e28f6e93b4fc6bf996a858632605cc7db1653676
karvinok <karvinokk@gmail.com>
2020-11-12 18:26:41 +0200

    Initial Commit

---
+++ SomeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeApp
+//
+//  Created by Олександр Палагнюк on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
